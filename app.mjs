0ec4bb9ea3d8c616dd3acb76619b67cc59c4223c
Nick Whitelegg <nickw4426@gmail.com>
2024-01-19 13:00:04 +0000

    Several enhancements

--- app.mjs
+++ app.mjs
@@ -12,26 +12,30 @@ app.get('/connect', async(req, res) => {
 
 });
 
-app.post('/connect', async(req, res) => {
-    console.log(req.body);
-    const response = await fetch(req.query.url, {
-        method: 'POST',
-        body: JSON.stringify(req.body),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    res.status(response.status).set({'Content-Type': 'text/plain'});
-    response.body.pipe(res);
-});
-
-
 app.delete('/connect', async(req, res) => {
-    const response = await fetch(req.query.url, {
+    const response = await fetch(req.body.url, {
         method: 'DELETE'
     });
     res.status(response.status).set({'Content-Type': 'text/plain'});
     response.body.pipe(res);
 });
+
+app.all('/connect', async(req, res) => {
+    if(["POST", "PUT"].indexOf(req.method) == -1) {
+        res.status(405).json({error:`Unrecognised method ${req.method}`});
+    } else {
+        const response = await fetch(req.body.url, {
+            method: req.method, 
+            body: req.body.contentType=='application/json' ? JSON.stringify(req.body.data): req.body.data,
+            headers: {
+                'Content-Type': req.body.contentType 
+            }
+        });
+        res.status(response.status).set({'Content-Type': 'text/plain'});
+        response.body.pipe(res);
+    }
+});
+
+
 
 app.listen(3200);    
--- public/index.js
+++ public/index.js
@@ -3,18 +3,24 @@ document.getElementById('send').addEventListener('click', async(e)=> {
     try {
         const url = document.getElementById('url').value;
         const method = document.getElementById('method').value;
-        const requestbody = document.getElementById('requestbody').value;
+        const requestBody = document.getElementById('requestBody').value;
+        const contentType = document.getElementById('contentType').value;
         let response;
         if(method == 'GET') {
             response = await fetch(`/connect?url=${url}`);
         } else {
-            response = await fetch(`/connect?url=${url}`, {
+            const request = {
+                data: contentType=='application/json' && method != "DELETE" ? JSON.parse(requestBody) : requestBody, 
+                contentType: contentType,
+                url: url
+            };
+            response = await fetch('/connect', {
                 method: method,
-                body: requestbody,
+                body: JSON.stringify(request),
                 headers: {
                     'Content-Type': 'application/json'
                 }
-            });
+            });    
         }
         const responseText = await response.text();
         document.getElementById('response').innerHTML = `<p><strong class='code${Math.floor(response.status/100)}'>HTTP Status Code ${response.status}</strong></p>`;
